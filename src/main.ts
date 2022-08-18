f2be0203cdc5cf99ca78077703fbd1bd00b1acdf
maaaaaaaaad <integral5137@gmail.com>
2022-08-18 20:49:02 +0900

    set prisma

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<string>('SERVER_PORT');
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   await app.listen(port, () => console.log(`START SERVER PORT: ${port}`));
 }
 bootstrap().then();
