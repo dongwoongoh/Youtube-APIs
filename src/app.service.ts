1cf2771aeb2a900ea37e50d1f164a066bd00d112
maaaaaaaaad <integral5137@gmail.com>
2022-08-20 22:36:16 +0900

    soejfosjef

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,6 +5,6 @@ import { PrismaService } from './prisma.service';
 export class AppService {
   constructor(private readonly prisma: PrismaService) {}
   getHello(): string {
-    return 'TEST!! at last';
+    return 'TEST!! at last change that schema fields';
   }
 }
