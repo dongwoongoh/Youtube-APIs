e9f030d8aa10d0c2b73cbb431830f6da0d7fb64e
maaaaaaaaad <integral5137@gmail.com>
2022-08-21 17:40:06 +0900

    add custom decorators that two it

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from './prisma.service';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   const configService = app.get(ConfigService);
   const port = configService.get<string>('SERVER_PORT');
   const prismaService = app.get(PrismaService);
