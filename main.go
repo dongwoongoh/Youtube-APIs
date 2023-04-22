f5c83ada6bebe4253c4a86115b1593d957dc75ef
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-22 09:10:54 +0000

    e

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
+func countNumbers() {
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Count:", i)
+		time.Sleep(1 * time.Second)
+	}
 }
+
+func printLetters() {
+	for char := 'a'; char <= 'e'; char++ {
+		fmt.Printf("Letter: %c\n", char)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	go countNumbers()
+	go printLetters()
+	time.Sleep(3 * time.Second)
+}
+
