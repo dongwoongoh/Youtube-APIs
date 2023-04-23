15eb73f423a44d4b2f8b248cdda0ff5c6446466d
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-23 07:24:32 +0000

    e

--- main.go
+++ main.go
@@ -8,17 +8,26 @@ import (
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+
 	ch := make(chan int)
+
+	start := time.Now()
+
 	for i := 0; i < 10; i++ {
 		go func() {
 			n := rand.Intn(100)
 			ch <- n
 		}()
 	}
+
 	sum := 0
 	for i := 0; i < 10; i++ {
 		sum += <-ch
 	}
 
+	end := time.Now()
+	elapsed := end.Sub(start)
+
 	fmt.Println("Sum:", sum)
+	fmt.Println("Elapsed time:", elapsed)
 }
