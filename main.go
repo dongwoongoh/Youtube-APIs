45b4a55fd76e4789673db8b122d0ce010351e856
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-26 05:16:52 +0000

    e

--- main.go
+++ main.go
@@ -4,60 +4,58 @@ import (
 	"sort"
 )
 
-type Pair struct {
-	genreName string
-	total     int
-	songs     []Song
+type Truck struct {
+	g, s, w, t, tot int64
 }
 
-type Song struct {
-	id    int
-	plays int
-}
+func check(trucks []Truck, target, a, b int64) bool {
+	gold, silver, both := int64(0), int64(0), int64(0)
 
-func solution(genres []string, plays []int) []int {
-	genreToSongs := make(map[string]*Pair)
-
-	for i := 0; i < len(genres); i++ {
-		genre := genres[i]
-		play := plays[i]
-		pair, exist := genreToSongs[genre]
-
-		if exist {
-			pair.total += play
-			pair.songs = append(pair.songs, Song{i, play})
-		} else {
-			genreToSongs[genre] = &Pair{genre, play, []Song{{i, play}}}
+	for _, truck := range trucks {
+		capacity := (target / (truck.t * 2)) * truck.w
+		if target >= truck.t {
+			capacity += truck.w
 		}
+		g := min(capacity, truck.g)
+		capacity -= g
+		s := min(capacity, truck.s)
+		gold += g
+		silver += s
+		both += min(g+s, truck.tot)
 	}
 
-	for _, pair := range genreToSongs {
-		sort.Slice(pair.songs, func(i, j int) bool {
-			if pair.songs[i].plays == pair.songs[j].plays {
-				return pair.songs[i].id < pair.songs[j].id
-			}
-			return pair.songs[i].plays > pair.songs[j].plays
-		})
+	if gold >= a && silver >= b {
+		return true
+	}
+	return both >= a+b
+}
+
+func min(x, y int64) int64 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func solution(a int, b int, g []int, s []int, w []int, t []int) int64 {
+	trucks := []Truck{}
+	for i := 0; i < len(g); i++ {
+		trucks = append(trucks, Truck{int64(g[i]), int64(s[i]), int64(w[i]), int64(t[i]), int64(g[i] + s[i])})
 	}
 
-	genresSorted := make([]*Pair, 0, len(genreToSongs))
-	for _, value := range genreToSongs {
-		genresSorted = append(genresSorted, value)
-	}
-
-	sort.Slice(genresSorted, func(i, j int) bool {
-		return genresSorted[i].total > genresSorted[j].total
+	sort.Slice(trucks, func(i, j int) bool {
+		return trucks[i].t < trucks[j].t
 	})
 
-	answer := []int{}
-	for _, pair := range genresSorted {
-		for i, song := range pair.songs {
-			if i > 1 {
-				break
-			}
-			answer = append(answer, song.id)
+	left, right := int64(0), int64(1e18)
+	for left < right {
+		mid := (left + right) / 2
+		if check(trucks, mid, int64(a), int64(b)) {
+			right = mid
+		} else {
+			left = mid + 1
 		}
 	}
 
-	return answer
+	return right
 }
