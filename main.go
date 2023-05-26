880595e7eeeb8ffdab161e0ff7fb0f6b7f3de225
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-26 05:07:43 +0000

    e

--- main.go
+++ main.go
@@ -1,23 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"sort"
+)
 
-func main() {
-	var n, k int
-	fmt.Scanf("%d %d\n", &n, &k)
+type Pair struct {
+	total int
+	songs []Song
+}
 
-	a := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scanf("%d\n", &a[i])
-	}
+type Song struct {
+	id    int
+	plays int
+}
 
-	var cnt int = 0
-	for i := n - 1; i >= 0; i-- {
-		cnt += k / a[i]
-		k %= a[i]
-		if k == 0 {
-			break
+func solution(genres []string, plays []int) []int {
+	genreToSongs := make(map[string]Pair)
+
+	for i := 0; i < len(genres); i++ {
+		genre := genres[i]
+		play := plays[i]
+		pair, exist := genreToSongs[genre]
+
+		if exist {
+			pair.total += play
+			pair.songs = append(pair.songs, Song{i, play})
+		} else {
+			genreToSongs[genre] = Pair{play, []Song{{i, play}}}
 		}
 	}
-	fmt.Println(cnt)
+
+	for _, pair := range genreToSongs {
+		sort.Slice(pair.songs, func(i, j int) bool {
+			if pair.songs[i].plays == pair.songs[j].plays {
+				return pair.songs[i].id < pair.songs[j].id
+			}
+			return pair.songs[i].plays > pair.songs[j].plays
+		})
+	}
+
+	genresSorted := make([]Pair, 0, len(genreToSongs))
+	for _, value := range genreToSongs {
+		genresSorted = append(genresSorted, value)
+	}
+
+	sort.Slice(genresSorted, func(i, j int) bool {
+		return genresSorted[i].total > genresSorted[j].total
+	})
+
+	answer := []int{}
+	for _, pair := range genresSorted {
+		for i, song := range pair.songs {
+			if i > 1 {
+				break
+			}
+			answer = append(answer, song.id)
+		}
+	}
+
+	return answer
 }
